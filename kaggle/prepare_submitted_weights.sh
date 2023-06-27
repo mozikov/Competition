32389ac9435538571468d4f9a80b9ce59da5d414
안희영 <78080866+AhnHeeYoung@users.noreply.github.com>
2023-06-27 13:30:12 +0900

    Create prepare_submitted_weights.sh

---
+++ kaggle/prepare_submitted_weights.sh
@@ -0,0 +1,15 @@
+kaggle datasets download -d ahnheeyoung1/2023-06-05-12-22-clip-50-200
+kaggle datasets download -d ahnheeyoung1/2023-06-08-12-22-clip50-200-ls03-resnet200
+kaggle datasets download -d ahnheeyoung1/2023-06-07-12-22-clip50-200-ls03-resnext101
+
+mkdir -p submitted_weights/2023-06-05-12-22-clip-50-200
+mkdir -p submitted_weights/2023-06-08-12-22-clip50-200-ls03-resnet200
+mkdir -p submitted_weights/2023-06-07-12-22-clip50-200-ls03-resnext101
+
+mv 2023-06-05-12-22-clip-50-200.zip submitted_weights/2023-06-05-12-22-clip-50-200/
+mv 2023-06-08-12-22-clip50-200-ls03-resnet200.zip submitted_weights/2023-06-08-12-22-clip50-200-ls03-resnet200/
+mv 2023-06-07-12-22-clip50-200-ls03-resnext101.zip submitted_weights/2023-06-07-12-22-clip50-200-ls03-resnext101/
+
+unzip submitted_weights/2023-06-05-12-22-clip-50-200/2023-06-05-12-22-clip-50-200.zip -d submitted_weights/2023-06-05-12-22-clip-50-200
+unzip submitted_weights/2023-06-08-12-22-clip50-200-ls03-resnet200/2023-06-08-12-22-clip50-200-ls03-resnet200.zip -d submitted_weights/2023-06-08-12-22-clip50-200-ls03-resnet200
+unzip submitted_weights/2023-06-07-12-22-clip50-200-ls03-resnext101/2023-06-07-12-22-clip50-200-ls03-resnext101.zip -d submitted_weights/2023-06-07-12-22-clip50-200-ls03-resnext101
