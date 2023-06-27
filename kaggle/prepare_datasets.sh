8123277cf23b691880947506d84090242b9bd566
안희영 <78080866+AhnHeeYoung@users.noreply.github.com>
2023-06-27 13:28:59 +0900

    Create prepare_datasets.sh

---
+++ kaggle/prepare_datasets.sh
@@ -0,0 +1,4 @@
+kaggle competitions download -c vesuvius-challenge-ink-detection
+mkdir vesuvius-challenge-ink-detection
+mv vesuvius-challenge-ink-detection.zip vesuvius-challenge-ink-detection/
+unzip vesuvius-challenge-ink-detection/vesuvius-challenge-ink-detection.zip -d vesuvius-challenge-ink-detection/
