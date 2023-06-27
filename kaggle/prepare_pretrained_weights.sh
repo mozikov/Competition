57b86ee5664172a14ac006139a6f21e0f71dba26
안희영 <78080866+AhnHeeYoung@users.noreply.github.com>
2023-06-27 13:29:41 +0900

    Create prepare_pretrained_weights.sh

---
+++ kaggle/prepare_pretrained_weights.sh
@@ -0,0 +1,7 @@
+gdown 1irIdC_v7wa-sBpTiBlsMlS7BYNdj4Gr7
+gdown 1HSuXzL9tUcJB-q972Gt28a77ri-KNwHY
+gdown 1nN-X7eCnPHuvmIwSRV9VxsesjNI7rizU
+mkdir pretrained_weights
+mv r3d152_KM_200ep.pth pretrained_weights
+mv r3d200_KM_200ep.pth pretrained_weights
+mv kinetics_resnext_101_RGB_16_best.pth pretrained_weights
